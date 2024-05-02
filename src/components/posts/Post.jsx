b74c8c3bd5a9ac43dda25059b260b0ab1f44dd9b
Marceline-G-S <Marceline.Silversmith@gmail.com>
2024-05-02 16:29:19 -0500

    Finished dropdown joins/name lists, added edit and delete functionality as well. That should've been its own branch, but oh well.

--- src/components/posts/Post.jsx
+++ src/components/posts/Post.jsx
@@ -1,16 +1,25 @@
 import { useEffect, useState } from "react"
 import { getUserById } from "../../services/userService.js"
+import { getTopicById } from "../../services/topicService.js"
 
 export const Post = ({post}) => {
   const [poster, setPoster] = useState({})
+  const [topic, setTopic] = useState({})
 
   useEffect(() => {
     getUserById(post.userId).then((poster0) => {setPoster(poster0[0])})
   }, [])
 
+  useEffect(()=>{
+    if(post?.topic){
+      getTopicById(post?.topic).then((topic0) => {setTopic(topic0)})
+    }
+}, [post])
+
 
   return <section className="post">
   <header className="post-info">#{post.id}</header>
+  <div>{topic?.topicName}</div>
   <div>{post.title}</div>
   <footer>
     <div>
--- src/components/profile/ProfileRender.jsx
+++ src/components/profile/ProfileRender.jsx
@@ -1,6 +1,16 @@
+import { useEffect, useState } from "react"
+import { getTypeById } from "../../services/typeService.js"
 import "./profile.css"
 
 export const ProfileRender = ({user}) => {
+    const [trainerType, setTrainerType] = useState("")
+
+    useEffect(()=>{
+        getTypeById(user?.type).then((typeObj)=>{
+            setTrainerType(typeObj)
+        })
+    }, [])
+
     return <div className="user">
                 <div className="user-info">
                     <div>Name</div>
@@ -8,7 +18,7 @@ export const ProfileRender = ({user}) => {
                 </div>
                 <div className="user-info">
                     <div>Trainer type:</div>
-                    <div>{user?.type}</div>
+                    <div>{trainerType.typeName}</div>
                 </div>
             </div>
 }
